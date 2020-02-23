37f5501f4f720a809c2f9d5b6c30c39743d697ef
Michael Fitzhavey <michael.fitzhavey@gmail.com>
2020-02-23 23:35:59 +0000

    fix: title

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 	 ** Headers of the page
 	 */
 	head: {
-		title: process.env.npm_package_name || 'Onewheel Selector',
+		title: 'Onewheel Selector',
 		meta: [
 			{ charset: 'utf-8' },
 			{
